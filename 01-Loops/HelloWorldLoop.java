a08c631062911ef821379b971c862be5bf218f3c
Kanani Het <info.kananihet@gmail.com>
2026-02-02 00:21:44 +0530

    HelloWordLoop file created

---
+++ 01-Loops/HelloWorldLoop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class HelloWorldLoop {
+    public static void main(String[] args) {
+        // print hello world n time (n taken from user)
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i=0; i<=num; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
