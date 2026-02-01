4907b899115264bc5a1d4fc26fc71e34625fc843
Kanani Het <info.kananihet@gmail.com>
2026-02-02 00:42:42 +0530

    Write code

---
+++ 01-Loops/PrintNum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class PrintNum {
+    public static void main(String[] args) {
+        // print 1 to 10
+
+        for(int i=0; i<11; i++) {
+            System.out.println(i);
+        }
+    }
+}
