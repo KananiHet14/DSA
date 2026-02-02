d6f1be2fa2845d7dbadd8ef17b4b14fbf41fbb7a
Kanani Het <info.kananihet@gmail.com>
2026-02-02 13:53:11 +0530

    1 to 10 by while loop

---
+++ 01-Loops/WhileLoops.java
@@ -0,0 +1,16 @@
+// print 1 to 10 by using while loop
+
+import java.util.*;
+
+public class WhileLoops {
+
+    public static void main(String[] args) {
+
+        int i=0;
+        while(i<11) {
+            System.out.println(i);
+            i = i+1; // or write i++;
+        }
+
+    }
+}
