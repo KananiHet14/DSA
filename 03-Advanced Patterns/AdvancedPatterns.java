51b6dc993eceb82efd18f1067b392ea8c61732d3
Kanani Het <info.kananihet@gmail.com>
2026-02-04 18:39:45 +0530

    5 patterns done

--- 03-Advanced Patterns/AdvancedPatterns.java
+++ 03-Advanced Patterns/AdvancedPatterns.java
@@ -60,6 +60,68 @@ public class AdvancePatterns {
                 }
                     System.out.println();
             }
+
+            02 Hollow rhombus
+
+            int n=5;
+            
+            for(int i=1; i<=n; i++) {
+            // spaces
+                for(int j=1; j<=n-i; j++) {
+                    System.out.print(" ");
+                }
+
+                // stars
+                for(int j=1; j<=5; j++) {
+                    if(i==1 || j==1 || i==n || j==n) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                        }
+                }
+                    System.out.println();
+            }
+
+
+
+            // 04 number pyramid
+
+            int n=5;
+            
+            for(int i=1; i<=n; i++) {
+            // spaces
+                for(int j=1; j<=n-i; j++) {
+                    System.out.print(" ");
+                }
+
+                // numbers print rows no , rows no times  
+                for(int j=1; j<=i; j++) {
+                    System.out.print(i + " ");
+                }
+                    System.out.println();
+            }
+
+
+            // 05 palindromic pattern
+
+            int n = 5;
+            for(int i=1; i<=n; i++) {
+                // spaces
+                for(int j=1; j<=n-i; j++) {
+                    System.out.print(" ");
+                }
+                    // 1st half numbers
+                    for(int j=i; j>=1; j--) {
+                     System.out.print(j);
+                    }
+
+                    // 2nd half numbers
+                    for(int j=2; j<=i; j++) {
+                     System.out.print(j);
+                    }
+
+                    System.out.println();
+            } 
     }
 
 }
