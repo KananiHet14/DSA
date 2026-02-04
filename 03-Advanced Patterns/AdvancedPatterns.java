d346c506f509ab4e1cdfffd1f96aebcb6e7b7238
Kanani Het <info.kananihet@gmail.com>
2026-02-04 18:47:03 +0530

    all patterns done

--- 03-Advanced Patterns/AdvancedPatterns.java
+++ 03-Advanced Patterns/AdvancedPatterns.java
@@ -122,6 +122,41 @@ public class AdvancePatterns {
 
                     System.out.println();
             } 
+
+
+
+
+
+
+            // 06 diamond pattern of stars\
+
+            int n = 5;
+
+            // upper half
+            for(int i=1; i<=n; i++) {
+                // spaces 
+                for(int j=1; j<=n-i; j++) {
+                    System.out.print(" ");
+                }
+                // stars
+                for(int j=1; j<=2*i-1; j++) {
+                    System.out.print("*");
+                }
+                    System.out.println();
+            }
+
+            // Lower half
+            for(int i=n; i>=1; i--) {
+                // spaces 
+                for(int j=1; j<=n-i; j++) {
+                    System.out.print(" ");
+                }
+                // stars
+                for(int j=1; j<=2*i-1; j++) {
+                    System.out.print("*");
+                }
+                    System.out.println();
+            }
     }
 
 }
