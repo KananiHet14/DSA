277bc1d3f992aa17d2357dc745f5db32ec8e34dc
Kanani Het <info.kananihet@gmail.com>
2026-02-05 21:31:53 +0530

    array declaration 2 way

---
+++ 05-Array/ProgramArray.java
@@ -0,0 +1,37 @@
+/* 
+1d array syntax = typed[] arrayName = new type[size];
+
+array is the group of same data typed
+
+array idexing is starting from 0.
+
+*/
+
+
+import java.util.*;
+
+public class ProgramArray {
+
+    public static void main(String[] args) {
+        int marks[] = new int[3];
+        marks[0] = 99; //phy
+        marks[1] = 98; //chem
+        marks[2] = 97; //bio
+
+        for(int i=0; i<3; i++) {
+            System.out.println(marks[i]);
+        }
+
+    }
+}
+
+/*
+2nd way to declare array
+
+int marks[] = { 99 , 98 , 97};
+
+for(int i=0; i<3; i++) {
+    System.out.println(marks[i]);
+}
+
+*/
