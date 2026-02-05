01fa60e9c554fa211bba08c7df9d98eb9397f894
Kanani Het <info.kananihet@gmail.com>
2026-02-05 16:09:25 +0530

    PrintMyName function written

--- 04-Functions & Methods/Functionss.java
+++ 04-Functions & Methods/Functionss.java
@@ -6,4 +6,28 @@ returnType FUnctionName(Type arg1, arg2, ......) {
     // operations
 }
 
+definations = function is a block of code which take some inputs and perform
+              some operations on inputs and after operations give us output.
+
+              public static void main(String[] args) {
+              
+              } this is also function where void used for no returns
+
 */ 
+
+import java.util.*;
+
+public class Functionss {
+
+    public static void PrintMyName(String name) {
+        System.out.println(name);
+        return ;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        PrintMyName(name);
+
+    }
+}
