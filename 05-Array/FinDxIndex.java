3bd0ba71a048ce3793387b6d3965506c3b7c622b
Kanani Het <info.kananihet@gmail.com>
2026-02-06 22:10:37 +0530

    all code comment

--- 05-Array/FinDxIndex.java
+++ 05-Array/FinDxIndex.java
@@ -1,21 +1,21 @@
-import java.util.*;
+// import java.util.*;
 
-public class FinDxIndex {
+// public class FinDxIndex {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int[] numbers = new int[size];
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int[] numbers = new int[size];
         
-        for(int i=0; i<size; i++) {
-            numbers[i] = sc.nextInt();
-        }
-        int x = sc.nextInt();
-        for(int i=0; i<numbers.length; i++) {
-            if(numbers[i] == x) {
-                System.out.println("array found at index : " + i);
-            } 
-            }
-        }
-    }
-// this is a algorithm bsed question whiuch name is "linear search"
+//         for(int i=0; i<size; i++) {
+//             numbers[i] = sc.nextInt();
+//         }
+//         int x = sc.nextInt();
+//         for(int i=0; i<numbers.length; i++) {
+//             if(numbers[i] == x) {
+//                 System.out.println("array found at index : " + i);
+//             } 
+//             }
+//         }
+//     }
+// // this is a algorithm bsed question whiuch name is "linear search"
--- 05-Array/ProgramArray.java
+++ 05-Array/ProgramArray.java
@@ -1,37 +1,37 @@
-/* 
-1d array syntax = typed[] arrayName = new type[size];
+// /* 
+// 1d array syntax = typed[] arrayName = new type[size];
 
-array is the group of same data typed
+// array is the group of same data typed
 
-array idexing is starting from 0.
+// array idexing is starting from 0.
 
-*/
+// */
 
 
-import java.util.*;
+// import java.util.*;
 
-public class ProgramArray {
+// public class ProgramArray {
 
-    public static void main(String[] args) {
-        int marks[] = new int[3];
-        marks[0] = 99; //phy
-        marks[1] = 98; //chem
-        marks[2] = 97; //bio
+//     public static void main(String[] args) {
+//         int marks[] = new int[3];
+//         marks[0] = 99; //phy
+//         marks[1] = 98; //chem
+//         marks[2] = 97; //bio
 
-        for(int i=0; i<3; i++) {
-            System.out.println(marks[i]);
-        }
+//         for(int i=0; i<3; i++) {
+//             System.out.println(marks[i]);
+//         }
 
-    }
-}
+//     }
+// }
 
-/*
-2nd way to declare array
+// /*
+// 2nd way to declare array
 
-int marks[] = { 99 , 98 , 97};
+// int marks[] = { 99 , 98 , 97};
 
-for(int i=0; i<3; i++) {
-    System.out.println(marks[i]);
-}
+// for(int i=0; i<3; i++) {
+//     System.out.println(marks[i]);
+// }
 
-*/
+// */
