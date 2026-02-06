60a8f3730fe0d7311e1aadb1ba6e2e9342bb3662
Kanani Het <info.kananihet@gmail.com>
2026-02-06 22:05:21 +0530

    String methods done

---
+++ 07-Strings/StringsMethods.java
@@ -0,0 +1,32 @@
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.*;
+public class StringsMethods {
+
+    public static void main(String[] args) {
+    // String concatination
+    String FN = "Kanani";
+    String SN = "Het";
+    String FULLN = FN + "@" + SN;
+    System.out.println(FULLN);
+
+    // string length
+    System.out.println(FULLN.length());
+    
+    // charAt
+    for(int i=0; i<FULLN.length(); i++) {
+        System.out.println(FULLN.charAt(i));
+    }
+
+    // compare
+    if(FN.compareTo(SN)==0) {
+        System.out.println("equal");
+    } else {
+        System.out.println("not equal");
+    }
+
+    // subsctring [indexing typed]
+    String sentence = "My name is kanani het";
+    String name = sentence.substring(11, sentence.length());
+    System.out.println(name);
+    }
+}
