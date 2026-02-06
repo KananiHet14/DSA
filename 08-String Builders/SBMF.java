14faf44b22c8af1aca7b02abd7403164863ebf89
Kanani Het <info.kananihet@gmail.com>
2026-02-06 22:49:35 +0530

    string builders operations and methods done

---
+++ 08-String Builders/SBMF.java
@@ -0,0 +1,32 @@
+// import java.util.*;
+
+// public class SBMF {
+//     public static void main(String[] args) {
+//         StringBuilder sb = new StringBuilder("KANANI");
+//         System.out.println(sb);
+
+//         // charAt for print string any character on display
+//         System.out.println(sb.charAt(3));
+
+//         // set character at index
+//         sb.setCharAt(0 ,'D');
+//         System.out.println(sb);
+
+//         // inster a new character
+//         StringBuilder name = new StringBuilder("het");
+//         name.insert(1, 'e');
+//         System.out.println(name);
+
+//         // delete the extra "e"
+//         name.delete(1,2);
+//         System.out.println(name);
+
+//         // append
+//         name.append("kanani");
+//         System.out.println(name);
+
+//         // length
+//         System.out.println(name.length());
+//     }
+    
+// }
