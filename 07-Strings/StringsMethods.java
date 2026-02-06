cd1dcd960f8e53e13d707fcbbd72b6590e51a12a
Kanani Het <info.kananihet@gmail.com>
2026-02-06 22:13:02 +0530

    all code comment

--- 07-Strings/StringsMethods.java
+++ 07-Strings/StringsMethods.java
@@ -1,31 +1,31 @@
-import java.util.*;
-public class StringsMethods {
+// import java.util.*;
+// public class StringsMethods {
 
-    public static void main(String[] args) {
-    // String concatination
-    String FN = "Kanani";
-    String SN = "Het";
-    String FULLN = FN + "@" + SN;
-    System.out.println(FULLN);
+//     public static void main(String[] args) {
+//     // String concatination
+//     String FN = "Kanani";
+//     String SN = "Het";
+//     String FULLN = FN + "@" + SN;
+//     System.out.println(FULLN);
 
-    // string length
-    System.out.println(FULLN.length());
+//     // string length
+//     System.out.println(FULLN.length());
     
-    // charAt
-    for(int i=0; i<FULLN.length(); i++) {
-        System.out.println(FULLN.charAt(i));
-    }
+//     // charAt
+//     for(int i=0; i<FULLN.length(); i++) {
+//         System.out.println(FULLN.charAt(i));
+//     }
 
-    // compare
-    if(FN.compareTo(SN)==0) {
-        System.out.println("equal");
-    } else {
-        System.out.println("not equal");
-    }
+//     // compare
+//     if(FN.compareTo(SN)==0) {
+//         System.out.println("equal");
+//     } else {
+//         System.out.println("not equal");
+//     }
 
-    // subsctring [indexing typed]
-    String sentence = "My name is kanani het";
-    String name = sentence.substring(11, sentence.length());
-    System.out.println(name);
-    }
-}
+//     // subsctring [indexing typed]
+//     String sentence = "My name is kanani het";
+//     String name = sentence.substring(11, sentence.length());
+//     System.out.println(name);
+//     }
+// }
