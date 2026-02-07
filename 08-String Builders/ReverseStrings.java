b0bc15232db1936dff080a8eb57b486e2dc6b407
Kanani Het <info.kananihet@gmail.com>
2026-02-07 13:24:21 +0530

    leet code problem solved

---
+++ 08-String Builders/ReverseStrings.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ReverseStrings {
+    public static void main(String[] args) {
+        // leet code Question
+        Scanner sc = new Scanner(System.in);
+        StringBuilder SB = new StringBuilder(sc.nextLine());
+
+        for(int i=0; i<SB.length()/2; i++) {
+            int front = i;
+            int back = SB.length() - 1 -i;
+
+            char frontchar = SB.charAt(front);
+            char backchar = SB.charAt(back);
+
+            SB.setCharAt(front, backchar);
+            SB.setCharAt(back, frontchar);
+
+        }
+        System.out.println(SB);
+    }
+}
