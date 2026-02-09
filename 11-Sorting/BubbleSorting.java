b152a14ab6b75dedacbce72a38990a971b6423cb
Kanani Het <tech.kananihet@gmail.com>
2026-02-09 17:46:07 +0530

    new file + code comment of past file

--- 11-Sorting/BubbleSorting.java
+++ 11-Sorting/BubbleSorting.java
@@ -1,28 +1,28 @@
-import java.util.*;
+// import java.util.*;
 
-public class BubbleSorting {
+// public class BubbleSorting {
 
-    public static void printArray(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+//     public static void printArray(int arr[]) {
+//         for(int i=0; i<arr.length; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
         
-        int arr[] = {7,8,3,1,2}; 
-        // bibble sort
-        for(int i=0; i<arr.length; i++) {
-            for(int j=0; j<arr.length-1-i; j++) {
-                if(arr[j] > arr[j+1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
-        printArray(arr);
-    }
-}
+//         int arr[] = {7,8,3,1,2}; 
+//         // bibble sort
+//         for(int i=0; i<arr.length-1; i++) {
+//             for(int j=0; j<arr.length-1-i; j++) {
+//                 if(arr[j] > arr[j+1]) {
+//                     // swap
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                 }
+//             }
+//         }
+//         printArray(arr);
+//     }
+// }
