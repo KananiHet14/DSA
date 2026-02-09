389eb1f71417a202ba1b10924f9d758795f886d4
Kanani Het <tech.kananihet@gmail.com>
2026-02-09 23:32:04 +0530

    Insertion Sorting of array

---
+++ 11-Sorting/InsertionSorting.java
@@ -0,0 +1,35 @@
+// import java.lang.reflect.Array;
+// import java.util.*;
+
+// public class InsertionSorting {
+
+//         // function
+//     public static void PrintArray(int arr[]) {
+//         for(int i=0; i<arr.length; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+
+//     public static void main(String[] args) {
+
+//         // Array
+//         int arr[] = {7,8,3,1,2};
+
+//         // Insertion Sorting
+//         for(int i=0; i<arr.length; i++) {
+//             int current = arr[i];
+//             int j = i-1;
+//             while(j >=0 && current < arr[j]) {
+//                 arr[j+1] = arr[j];
+//                 j--;
+//             }
+
+//             // Placement
+//             arr[j+1] = current;
+
+//         }
+//         PrintArray(arr);
+//     }
+
+// }
